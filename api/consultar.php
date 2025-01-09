9e4154b32a142936555f653b02513a9d052a1db3
GeorginaTL <georginatelloleon@gmail.com>
2025-01-08 18:46:10 -0700

    sistemaphp1

--- api/consultar.php
+++ api/consultar.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
+//header("Access-Control-Allow-Origin: http://127.0.0.1:5500");
 /*
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
